6c12266a064385917fe169c11256a3afecfb7d15
natgsarabia <natalia.garcia@estudiant.fjaverianas.com>
2024-05-17 12:54:59 +0200

    .

--- PROYECTO_LENGUAJE_MARCAS/obj/Debug/net8.0/PROYECTO.AssemblyInfo.cs
+++ PROYECTO_LENGUAJE_MARCAS/obj/Debug/net8.0/PROYECTO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROYECTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d8461d7dfc8606f34455b5a41b73200efa512c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f3d78d612adf4afe4aa4fd7d628b201f41b411")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROYECTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROYECTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
