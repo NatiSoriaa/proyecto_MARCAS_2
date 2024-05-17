05b2cbc15ad2daac7c87af83907764f61f59b839
natgsarabia <natalia.garcia@estudiant.fjaverianas.com>
2024-05-17 13:08:32 +0200

    fotos fondo

--- PROYECTO_LENGUAJE_MARCAS/obj/Debug/net8.0/PROYECTO.AssemblyInfo.cs
+++ PROYECTO_LENGUAJE_MARCAS/obj/Debug/net8.0/PROYECTO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROYECTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f3d78d612adf4afe4aa4fd7d628b201f41b411")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fba1a73389789ccc762005e0c61a71744c6ed3f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROYECTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROYECTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
