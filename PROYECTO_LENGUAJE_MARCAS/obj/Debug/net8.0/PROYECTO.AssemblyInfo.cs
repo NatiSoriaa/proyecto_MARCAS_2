ba6af9fb65973041ca73d4858a34c1e1829ad37d
natgsarabia <natalia.garcia@estudiant.fjaverianas.com>
2024-05-20 19:55:26 +0200

    add señal para indicar que existe el link aqi air

--- PROYECTO_LENGUAJE_MARCAS/obj/Debug/net8.0/PROYECTO.AssemblyInfo.cs
+++ PROYECTO_LENGUAJE_MARCAS/obj/Debug/net8.0/PROYECTO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROYECTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7876795c38576cf70f088b3b57cd5c4d61fa3a54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f59c66d9a5d30c9717b0ad0f7e8d75fb8ef09dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROYECTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROYECTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
